94e521e572defa5b55447cdfd7689d79b340e713
eskildt <eskildtangen@gmail.com>
2019-09-18 12:30:00 +0200

    Han sier trykk ctrl k, men  det er egentlig cmd k din dust

---
+++ src/test/java/no/httpfilerTest/www.java
@@ -0,0 +1,13 @@
+package no.httpfilerTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class www {
+
+    @Test
+    void mathShouldWork(){
+        assertEquals(4, 2+2);
+    }
+}
